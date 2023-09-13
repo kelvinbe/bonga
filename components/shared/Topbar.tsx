38d2d7be89d0be0bbe83d6802866167ecbfb1d1e
kelvinbe <kelvin.beno.k@gmail.com>
2023-09-13 10:45:36 +0300

    updates to code

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,18 +1,34 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 
 const Topbar = () => {
   return (
     <nav className='topbar'>
-        
-        Topbar
         <Link href='/' className='flex items-center gap-4'>
-            <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+            <Image src="/assets/assets/logo.svg" alt="logo" width={28} height={28} />
             <p className='text-heading3-bold text-light-1 max-xs:hidden'> Bonga </p>
 
         </Link>
-        
+        <div className='flex items-center gap-1'>
+          <div className='block md:hidden'>
+            <SignedIn>
+                <SignOutButton>
+                  <div className='flex cursor-pointer'>
+                    <Image src='/assets/assets/logout.svg' alt='logout' width={24} height={24} />
+                  </div>
+                </SignOutButton>
+            </SignedIn>
+          </div>
+          <OrganizationSwitcher
+          appearance={{
+            elements: {
+            organizationSwitcherTrigger:"py-2 px-4"
+            }
+          }}
+          />
+        </div>
         </nav>
   )
 }
